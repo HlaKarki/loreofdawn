4da26d4a66cabe754ec91b9a9748f91ad33eee8e
hlakarki <kshap.karki@gmail.com>
2025-11-09 20:05:33 -0500

    fix: production cors issues

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,6 +1,8 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+export default clerkMiddleware({
+	authorizedParties: ["https://loreofdawn.com", "https://auth.loreofdawn.com"],
+});
 
 export const config = {
 	matcher: [
