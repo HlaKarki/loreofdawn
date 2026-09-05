d62c9ae0a35344aca380257e758d2e964e84d366
Hla Htun <kshap.karki@gmail.com>
2026-09-04 20:51:25 -0400

    chore: web; drop unused landing stubs

--- apps/web/src/features/landing/didYouKnow.tsx
+++
@@ -1,8 +0,0 @@
-export const DidYouKnow = () => {
-	return (
-		<div>
-			<h1>Did you know?</h1>
-			<h4>Potentially send user to wiki trivia page or nudge them to ask the ai question?</h4>
-		</div>
-	);
-};
