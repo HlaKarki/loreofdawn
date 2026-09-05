0179050b25eec7ae762262e946c498e75f400f27
Hla Htun <kshap.karki@gmail.com>
2026-09-04 20:35:42 -0400

    fix: web; restore clerk authorizedParties, theme font vars, drop unused deps

--- apps/web/src/start.ts
+++ apps/web/src/start.ts
@@ -2,5 +2,14 @@ import { clerkMiddleware } from "@clerk/tanstack-react-start/server";
 import { createStart } from "@tanstack/react-start";
 
 export const startInstance = createStart(() => ({
-	requestMiddleware: [clerkMiddleware()],
+	requestMiddleware: [
+		clerkMiddleware({
+			authorizedParties: [
+				"https://cf-api.loreofdawn.com",
+				"https://loreofdawn.com",
+				"https://auth.loreofdawn.com",
+				"http://localhost:1201",
+			],
+		}),
+	],
 }));
